04d75455dc0ea0c1697894010e78bbdc32620599
DevSazal <sajal.efran@gmail.com>
2018-12-24 09:02:58 +0600

    model alter and migrate for supplier BType

--- resources/views/admin/businesstype/edit.blade.php
+++ resources/views/admin/businesstype/edit.blade.php
@@ -23,7 +23,7 @@
         <!-- Horizontal Form -->
         <div class="box box-info">
           <div class="box-header with-border">
-            <h3 class="box-title">Edit </h3>
+            <h3 class="box-title">Edit</h3>
           </div>
           <!-- /.box-header -->
           <!-- form start -->
