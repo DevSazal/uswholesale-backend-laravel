657724282538fce63b08042ef9ad13c8acb01738
DevSazal <sajal.efran@gmail.com>
2019-01-05 17:59:16 +0600

    Alter model for adding new column in BuyerPost

---
+++ database/migrations/2019_01_05_115801_add_sub_category_id_to_buyer_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSubCategoryIdToBuyerPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('buyer_posts', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('buyer_posts', function (Blueprint $table) {
+            //
+        });
+    }
+}
