eaa4191b2f3c7fb0c3a75ea07bccc55cfaadddbe
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2019-06-07 15:07:38 +0800

    Create a component for search form.

---
+++ resources/views/components/search.blade.php
@@ -0,0 +1,67 @@
+<!-- _______Starting of search_________  -->
+<section id="home-search">
+    <div class="wrap">
+        <form name="searchForm" method="get" action="" onsubmit="">
+            <select id="act-select" name="action" class="selectBox-dropdown">
+                <option value="suppliers">Search Suppliers</option>
+                <option value="products">Search Products</option>
+            </select>
+            <div class="text">
+              <input type="text" autocomplete="off" id="q" name="q" value="" placeholder="Explore millions of Wholesale Products">
+            </div>
+            <input type="submit" value="Search" class="sbutton">
+            <!-- <div class="suggest">
+                <span class="suggest__tag">
+        Related Searches:
+    </span>
+                <ul class="suggest__list u-margin-top-small">
+                    <li class="suggest__item">
+                        <a href="#" class="suggest__link">children's skiing apparel</a>
+                    </li>
+                    <li class="suggest__item">
+                        <a href="#" class="suggest__link">children's skiing apparel</a>
+                    </li>
+                    <li class="suggest__item">
+                        <a href="#" class="suggest__link">children's skiing apparel</a>
+                    </li>
+                    <li class="suggest__item">
+                        <a href="#" class="suggest__link">children's skiing apparel</a>
+                    </li>
+                    <li class="suggest__item">
+                        <a href="#" class="suggest__link">children's skiing apparel</a>
+                    </li>
+                </ul>
+            </div> -->
+        </form>
+    </div>
+</section>
+<!-- _______Ending of search_________  -->
+
+@section('scripts')
+  <script src="{{ asset('asset/js/typeahead.min.js') }}"></script>
+  <script>
+    $(function() {
+      $('#q').typeahead({
+        source: function(query, process){
+          let path = "{{ route('autocomplete_search') }}"
+          let action = $("[name='action']").children('option:selected').val()
+
+          return $.get(path, { query : query, action : action }, function (data) {
+                    return process(data);
+                 })
+          /*
+          url: '{{ route("autocomplete_search") }}?action=%ACTION&q=%QUERY',
+          replace: (url, uriEncodedQuery) => {
+
+            return url.replace('%QUERY', uriEncodedQuery).replace('%ACTION', action)
+          }
+          */
+        }
+      })
+
+      $('[name="searchForm"]').on('submit', function(e) {
+
+      })
+    })
+  </script>
+@endsection
