4dc3aa89f80a7aa4c102af1bbe7b81b205754b5b
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2019-06-09 01:47:07 +0800

    Form design fix.

--- resources/views/paymentCheck.blade.php
+++ resources/views/paymentCheck.blade.php
@@ -5,11 +5,63 @@
 <title>Auth</title>
 @endsection
 
+@section('script')
+  <style>
+  /**
+ * The CSS shown here will not be introduced in the Quickstart guide, but shows
+ * how you can use CSS to style your Element's container.
+ */
+.StripeElement {
+  box-sizing: border-box;
+
+  height: 40px;
+
+  padding: 10px 12px;
+
+  border: 1px solid transparent;
+  border-radius: 4px;
+  background-color: white;
+
+  box-shadow: 0 1px 3px 0 #e6ebf1;
+  -webkit-transition: box-shadow 150ms ease;
+  transition: box-shadow 150ms ease;
+}
+
+.StripeElement--focus {
+  box-shadow: 0 1px 3px 0 #cfd7df;
+}
+
+.StripeElement--invalid {
+  border-color: #fa755a;
+}
+
+.StripeElement--webkit-autofill {
+  background-color: #fefde5 !important;
+}
+  </style>
+@endsection
+
 @section('content')
   <main>
     <section class="section-auth">
         <div class="container">
-            <h1>This is Payment Check Page</h1>
+            <h1>Subscription Payment Checkout</h1>
+
+            <div>
+              Your selected plan is
+              <strong class="text-info">
+              @switch(auth()->user()->payment)
+                @case(1)
+                  Basic
+                  @break
+                @case(2)
+                  Standard
+                  @break
+                @default
+                  Premium
+              @endswitch
+              </strong>
+            </div>
 
             <form action="{{ route('charge') }}" method="post" id="payment-form">
               @csrf
